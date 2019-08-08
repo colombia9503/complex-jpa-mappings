89622bc75ab8b39c4fcc3c3df87f5afcec669470
sumana <s.umana.villada@accenture.com>
2019-08-08 11:34:35 -0500

    first commit

---
+++ src/main/java/dev/sumana/cmappings/ComplexMappingsPocApplication.java
@@ -0,0 +1,13 @@
+package dev.sumana.cmappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexMappingsPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplexMappingsPocApplication.class, args);
+    }
+
+}
